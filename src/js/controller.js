e7bdf9ccbf20894f19dfdac32473dc0fc5b7d4f5
Tomasz Posiadala <tomasz.posiadala@jaywing.com>
2021-11-09 22:08:11 +0000

    setup

---
+++ src/js/controller.js
@@ -0,0 +1,19 @@
+// const sgMail = require('@sendgrid/mail');
+
+// const SENGRID_API_KEY =
+//   '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';
+
+// sgMail.setApiKey(SENGRID_API_KEY);
+
+// const message = {
+//   to: 'tomaszposiadala@gmail.com',
+//   from: 'tomaszposiadala@gmail.com',
+//   subject: 'Hello Cypress test email',
+//   text: 'Hello test test',
+//   html: '<h1>Hello test test</h1>',
+// };
+
+// sgMail
+//   .send(message)
+//   .then((response) => console.log('Email sent...'))
+//   .catch((error) => console.log(error.message));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const outputDir = path.resolve(__dirname, 'dist/js');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: [
+    'regenerator-runtime/runtime.js',
+    path.resolve(__dirname, 'src/js/controller.js'),
+  ],
+  output: {
+    path: outputDir,
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
