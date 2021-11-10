7c2ff2b387631b944f9b2fad5789c217c2cbd6f8
Tomasz Posiadala <tomasz.posiadala@jaywing.com>
2021-11-10 11:13:07 +0000

    removed recaptcha

---
+++ cypress/integration/tests/form.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+description('Email Form Test', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/commands/viewport');
+  });
+
+  it('cy.viewport() - set the viewport size and dimension', () => {});
+});
